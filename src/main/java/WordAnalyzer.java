70db1e9b4a2efb2a7dcce0d849ad02cb82cd54ec
Phan Cong <phancong7424@gmail.com>
2026-04-14 14:03:29 +0700

    first commit

---
+++ src/main/java/WordAnalyzer.java
@@ -0,0 +1,15 @@
+public class WordAnalyzer {
+    public int countTargetWord(String[] words, String targetWord) {
+        if (words == null || targetWord == null) {
+            return 0;
+        }
+
+        int count = 0;
+        for (String word : words) {
+            if (word.equalsIgnoreCase(targetWord)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
